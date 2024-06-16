761b8ff6717955d004228fbce0a5b8094fe3d437
Jobi S S <jobisjobi1234@gmail.com>
2024-06-16 11:19:42 +0530

    Initial commit

---
+++ camel_case_parser/parser.py
@@ -0,0 +1,66 @@
+import re
+
+def camel_to_snake(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+def snake_to_camel(name):
+    components = name.split('_')
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+
+
+def dict_snake(my_dict: dict):
+    new_dict = {}
+    for (key, val) in my_dict.items():
+        if isinstance(key, int):
+            new_dict.update({key:val})
+            continue
+        # print(key, val)
+        new_dict.update({camel_to_snake(key):val})
+    return new_dict
+
+def list_dict_snake(data: list[dict]):
+    """
+    Request data : type -> List of dictionary
+
+    """
+    result_list = []
+    for dict_data in data:
+        
+        try:
+            if not isinstance(dict_data, dict):
+                raise TypeError(f"List have a unprocessed type of {type(dict_data)}, except type of 'dict'")
+            parsed_data = dict_snake(dict_data)
+        except TypeError as e:
+            raise TypeError(f"{e}")
+        except Exception:
+            raise RuntimeError("Something must gone wrong")
+        
+        result_list.append(parsed_data)
+    return result_list
+
+
+my_dict = {
+    'firstName': 'Jobi',
+    'lastName': 'Eren',
+    '123':'123',
+    1:'1234'
+}
+
+my_list_dict = [
+    {
+    'firstName': 'Jobi',
+    'lastName': 'Eren',
+    '123':'123',
+    1:'1234'
+},
+{
+    'firstName': 'Whoever',
+    'lastName': 'whatever'
+},
+"eren yeager"
+]
+
+print(dict_snake(my_dict))
+print(list_dict_snake(my_list_dict))
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='camel_case_parser',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    tests = 'tests',
+    requires=[],
+    package_dir=[]
+
+)
